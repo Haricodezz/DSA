ad82c3e83f8202d4d85a6b81118ac697c0a8023c
Hari Prakash Yadav <hariprakash7059@gmail.com>
2026-08-16 19:07:04 +0530

    Time: 2 ms (15.20%) | Memory: 9.7 MB (63.19%) - LeetSync

---
+++ Array/Mid/118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+vector<int> genRow(int row){
+long long ans=1;
+vector<int> ansRow;
+
+ansRow.push_back(ans);
+
+for(int i =1;i < row; i++){
+    ans=ans*(row-i);
+    ans=ans/i;
+    ansRow.push_back(ans);
+}
+
+return ansRow;
+
+}
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> ans;
+        for(int i =1;i<=numRows;i++){
+            ans.push_back(genRow(i));
+        }
+return ans;
+    }
+};
